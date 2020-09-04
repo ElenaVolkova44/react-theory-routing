0f696b0c80c5358ecdbf20c06022a929726b6d07
ee.volkova1 <ee.volkova1@tensor.ru>
2020-09-04 19:07:15 +0300

    routes:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.scss';
+import {Route, NavLink, Switch, Redirect} from "react-router-dom";
+import Cars from './Cars/Cars';
+import About from "./About/About";
+import CarDetail from "./CarDetail/CarDetail";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+   state = {
+      isLoggedIn: false
+   };
+
+   render() {
+      return (
+         <div className={classes.App}>
+            <nav className={classes.nav}>
+               <ul>
+                  <li>
+                     <NavLink exact activeClassName={classes.active} to="/">Home</NavLink>
+                  </li>
+                  <li>
+                     <NavLink activeClassName={classes.active} to="/about">About</NavLink>
+                  </li>
+                  <li>
+                     <NavLink
+                        activeClassName={classes.active}
+                        to={{
+                           pathname: '/cars/'
+                        }}
+                     >
+                        Cars
+                     </NavLink>
+                  </li>
+               </ul>
+            </nav>
+
+            <hr/>
+            <div style={{textAlign: 'center'}}>
+               <h3>Is Logged In {String(this.state.isLoggedIn).toUpperCase()}</h3>
+               <button onClick={() => this.setState({isLoggedIn: true})}>Login</button>
+            </div>
+            <hr/>
+            <Switch>
+               <Route path="/" exact render={() => <h1>HomePage</h1>}/>
+               {this.state.isLoggedIn && <Route path="/about" exact component={About}/>}
+               <Route path="/cars/:name" component={CarDetail}/>
+               <Route path="/cars" component={Cars}/>
+               <Redirect to={'/'}/>
+               {/*<Route render={() => {
+               return (
+                  <h1 style={{color: 'red', textAlign: 'center'}}>404 not found</h1>
+               )
+            }}/>*/}
+            </Switch>
+         </div>
+      );
+   }
 }
 
 export default App;
